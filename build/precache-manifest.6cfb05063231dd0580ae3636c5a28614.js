181b5b64fbe179349ec39805c911e2dd3dc292df
Majd Hailat <majdhailat64@gmail.com>
2020-10-12 17:36:28 -0400

    add build

---
+++ build/precache-manifest.6cfb05063231dd0580ae3636c5a28614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411e2d2099ec4e108e9616cec3c2fa9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2691da56829931ae699a",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3c3d6769542078f07dde",
+    "url": "/static/css/main.90dc776f.chunk.css"
+  },
+  {
+    "revision": "2691da56829931ae699a",
+    "url": "/static/js/2.0627ad81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0627ad81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3d6769542078f07dde",
+    "url": "/static/js/main.8a2f0d65.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
